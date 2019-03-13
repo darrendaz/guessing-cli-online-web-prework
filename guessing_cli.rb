0156cdc22167e1b95e8de3a7696071999ad0c658
Darren L <darren.lou@gmail.com>
2019-03-13 03:04:34 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 require "pry"
 def run_guessing_game
   loop_continue = true
-  while (loop_continue) do
+  while loop_continue do
     number = rand(6)
     puts "Guess a number between 1 and 6:"
     guess = gets.chomp
